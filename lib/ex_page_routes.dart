8aaccae6ee7cea952844cea7a38588a7f22f5cb0
blood <hey-blood@foxmail.com>
2021-12-02 03:55:03 +0800

    Route Replacement & Clear Route Stack

--- lib/ex_page_routes.dart
+++ lib/ex_page_routes.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyPageRouteApp extends StatelessWidget {
@@ -14,11 +13,16 @@ class MyPageRouteApp extends StatelessWidget {
           return _buildPageRoute(arg);
         }
       }, */
+      // FIXME only initialRoute or home
+      initialRoute: '/home',
+      // home: HomePage(),
       routes: {
+        '/home': (context) => HomePage(),
         '/first': (context) => FirstPage(),
         '/second': (context) => SecondPage(),
+        '/third': (context) => ThirdPage(),
+        '/fourth': (context) => FourthPage(),
       },
-      home: FirstPage(),
     );
   }
 }
@@ -34,6 +38,25 @@ Route _buildPageRoute(Object? arg) {
   );
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Next"),
+          onPressed: () => Navigator.pushNamed(context, '/first'),
+        ),
+      ),
+    );
+  }
+}
+
 class FirstPage extends StatelessWidget {
   const FirstPage({Key? key}) : super(key: key);
 
@@ -44,23 +67,33 @@ class FirstPage extends StatelessWidget {
         title: Text("First Page"),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text("Next"),
-          onPressed: () => {
-            // Navigator.push(context, _buildPageRoute()),
-            // or
-            // 1. use a ios style page route
-            // CupertinoPageRoute(builder: (context) => SecondPage()),
-            // 2. use a Material style page route
-            // MaterialPageRoute(builder: (context) => SecondPage()),
-            // 3. use custom page route
-            // Navigator.of(context).push(_buildPageRoute()),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              child: Text("Back"),
+              onPressed: () => Navigator.pop(context),
+            ),
+            ElevatedButton(
+              child: Text("Next"),
+              onPressed: () => {
+                // Navigator.push(context, _buildPageRoute()),
+                // or
+                // 1. use a ios style page route
+                // CupertinoPageRoute(builder: (context) => SecondPage()),
+                // 2. use a Material style page route
+                // MaterialPageRoute(builder: (context) => SecondPage()),
+                // 3. use custom page route
+                // Navigator.of(context).push(_buildPageRoute()),
 
-            // use named page route
-            Navigator.pushNamed(context, '/second',
-                //  pass arguments to second page
-                arguments: "args of page"),
-          },
+                // use named page route
+                Navigator.pushNamed(context, '/second',
+                    //  pass arguments to second page
+                    arguments: "args of second page"),
+              },
+            ),
+          ],
         ),
       ),
     );
@@ -81,12 +114,82 @@ class SecondPage extends StatelessWidget {
         title: Text(arg as String),
       ),
       body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              child: Text("Go to third page"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/third');
+              },
+            ),
+            ElevatedButton(
+              child: Text("Replace with third page"),
+              onPressed: () {
+                // go to last page
+                /* Navigator.pop(context);
+                // or
+                Navigator.of(context).pop(); */
+
+                Navigator.pushReplacementNamed(context, '/third');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Third Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              child: Text("Back to last"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            ElevatedButton(
+              child: Text("Go to fourth page"),
+              onPressed: () {
+                // Clear route stack, and back route will be ModalRoute.withName()
+                Navigator.pushNamedAndRemoveUntil(
+                    context, '/fourth', ModalRoute.withName('/first'));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FourthPage extends StatelessWidget {
+  const FourthPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fourth Page"),
+      ),
+      body: Center(
         child: ElevatedButton(
-          child: Text("GO BACK"),
+          child: Text("Back to first"),
           onPressed: () {
-            // Navigator.pop(context);
-            // or
-            Navigator.of(context).pop();
+            // it will back to initialRoute, because the route stack is empty
+            Navigator.pop(context);
           },
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          alignment: Alignment.center,
-          child: Text("Hello Flutter!"),
+      home: MyFirstPage(),
+    );
+  }
+}
+
+class MyFirstPage extends StatelessWidget {
+  const MyFirstPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppBar title'),
+        centerTitle: true,
+        elevation: 20,
+        toolbarHeight: 70,
+        toolbarOpacity: 0.7,
+        backgroundColor: Colors.green,
+        leadingWidth: 100,
+        leading: Container(
+          color: Colors.red,
+          child: IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Clicked appbar leading')),
+              );
+            },
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.more),
+            tooltip: 'More',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Clicked action More')),
+              );
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Settings',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Clicked action Settings')),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Hello Flutter!"),
+            ElevatedButton(
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => MySecondPage())),
+              child: Text("Next"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MySecondPage extends StatelessWidget {
+  const MySecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Second Page"),
+        automaticallyImplyLeading: true,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("My Second Page"),
         ),
       ),
     );
